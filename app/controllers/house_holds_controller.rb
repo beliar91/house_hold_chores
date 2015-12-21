702ac2a577584c7f51aebae53672105eae48a4af
beliar91 <beliar91@vp.pl>
2015-12-21 22:13:01 +0100

    Working CRUD for House Holds

---
+++ app/controllers/house_holds_controller.rb
@@ -0,0 +1,44 @@
+class HouseHoldsController < ApplicationController
+
+  before_action :set_house_hold, only: [:show, :edit, :update, :destroy]
+
+
+  def new
+    @house_hold = HouseHold.new
+  end
+
+  def index
+    @house_holds = HouseHold.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @house_hold = HouseHold.create(house_hold_params)
+
+    redirect_to @house_hold if @house_hold.save
+  end
+
+  def update
+    redirect_to @house_hold, notice: 'House Hold was successfully updated' if @house_hold.update(house_hold_params)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_house_hold
+    @house_hold = HouseHold.find(params[:id])
+  end
+
+  def house_hold_params
+    params.require(:house_hold).permit(:name)
+  end
+
+
+end
