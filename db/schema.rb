4fa2e41c256997a1d28f4a950787808cd31cd176
beliar91 <beliar91@vp.pl>
2016-04-09 21:07:57 +0200

    Typos fixed and changes in tasks/new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403112821) do
+ActiveRecord::Schema.define(version: 20160409175843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 20160403112821) do
     t.datetime "updated_at",                          null: false
     t.integer  "house_hold_id"
     t.boolean  "complete",            default: false, null: false
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
